b5b90abb9ce4a5ac22921416cafd88dfc2698b2b
Muhammad Usman Awan <uawan8154@gmail.com>
2025-12-16 16:01:48 +0500

    feat: add controller and route for post api for create room

---
+++ controllers/room.controller.js
@@ -0,0 +1,27 @@
+import { createRoomCore } from "../sockets/roomEvents/createRoom.service.js";
+import { rooms, saveTimeouts } from "../sockets/helpers.js";
+
+export const createRoom = async (req, res) => {
+  try {
+    const { roomId, username, mode } = req.body;
+
+    const { response } = await createRoomCore({
+      rooms,
+      saveTimeouts,
+      roomId,
+      username,
+      mode,
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: response,
+    });
+  } catch (err) {
+    console.error("API createRoom error:", err);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
